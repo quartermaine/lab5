29ba56cccff58d7d24bc4fae2c47b49cfc42add5
quartermaine <andreas_xx@aol.com>
2018-10-03 12:37:44 +0200

    add

--- R/geocode_func.R
+++ R/geocode_func.R
@@ -1,28 +1,17 @@
-#' @title Coordinates finder
+#' Coordinates finder
 #'
-#' @description The 'geocode_func' function is a class that converts an address to coordinates.
+#' The 'geocode_func' function is a class that converts an address to coordinates.
 #'
-#' @param data A data frame of character string addreses
-#'
-#' @return A single coordinate or a dataframe cointaining full address names as well as the coordinates
+#' @field F A data frame
 #'
 #' @import methods
 #' @importFrom httr content GET
 #' @importFrom rjson fromJSON
 #'
-#' @examples
-#' e <- geocode_func()
-#' e$getCoordinates( "Mobile, Alabama, United States")
-#'
-#' e$getCorrdinates(addresses)
-#'
-#' @export
+#' @export geocode_func
 #' @exportClass geocode_func
 
-geocode_func <- setRefClass("geocode_func",
-                            fields = list(
-                              f="data.frame"
-                            ),
+geocode_func <- setRefClass("geocode_func",fields = list(f="data.frame"),
                             methods = list(
                               initialize = function(){
                                 cat("User::initialize")
@@ -32,7 +21,7 @@ geocode_func <- setRefClass("geocode_func",
                                 if (is.character(data)){
                                   data <- as.data.frame(data)
                                 }
-                                if(data=="" | grepl("[][!#$%()*.:;<=>@^_`|~.{}]", data[,1])==TRUE){
+                                if(data=="" || grepl("[][!#$%()*.:;<=>@^_`|~.{}]", data[,1])==TRUE){
                                   stop("Please check again your input")
                                 }
                                 a<-vector("character")
@@ -40,6 +29,7 @@ geocode_func <- setRefClass("geocode_func",
                                 d<-vector("numeric")
                                 for(i in 1:nrow(data)){
                                   print("Processing")
+                                  # key <- "AIzaSyCYgKKt2fn7Crt-V6Hnc5aw5lSfy7XLQ-Y"
                                   url<- "http://www.datasciencetoolkit.org/maps/api/geocode/json"
                                   json <-rjson::fromJSON(httr::content(httr::GET(url,query=list(sensor="FALSE",address=as.character(data[i,1]))),type="text"))
                                   if (json$status=="OK"){
